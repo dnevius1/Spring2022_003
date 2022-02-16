c2b1919fb8710a0a5b9608378f0b21ff93ddc109
Daniel Nevius <71950440+dnevius1@users.noreply.github.com>
2022-02-16 15:16:23 -0600

    Update TravelDestination.java
    Changed the name and destination in the print message.

--- TravelDestination.java
+++ TravelDestination.java
@@ -2,7 +2,7 @@
 public class TravelDestination {
 
 	public static void main(String[] args) {
-				System.out.println("My name is Heena Rathore. I wish to travel to Hawaii one day !");
+				System.out.println("My name is Daniel Nevius. I wish to travel to Paris one day !");
 
 	}
 
