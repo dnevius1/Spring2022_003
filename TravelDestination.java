dc90b66c8b48b05d3b504fb7e9ae717d7b2421fb
Daniel Nevius <daniel.nevius@my.utsa.edu>
2022-02-21 14:38:45 -0600

    making some changes

--- TravelDestination.java
+++ TravelDestination.java
@@ -2,7 +2,7 @@
 public class TravelDestination {
 
 	public static void main(String[] args) {
-				System.out.println("My name is Daniel Nevius. I wish to travel to Paris one day !");
+				System.out.println("My name is Daniel Nevius. I wish to travel to Paris, France one day !");
 
 	}
 
